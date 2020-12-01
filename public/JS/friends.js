f8be17d86435b4c5edd00c6c50270c81b283946b
Farrukh <44077833+farfar336@users.noreply.github.com>
2020-12-01 16:10:42 -0700

    Update friends.js

--- public/JS/friends.js
+++ public/JS/friends.js
@@ -1,3 +1,5 @@
+//socket, userID, displayName, userType, viewedUserID are global variables that are initialized in index.html, near the top of the file
+
 $(function () {
   $('#friendsToHomeButton').click(function(){
     $('#friends').hide();
